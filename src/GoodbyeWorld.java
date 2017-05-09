9df5e3db6f9391b28f2ea69e74ed5c3bd605755f
jburge <jburge@coloradocollege.edu>
2017-05-08 19:03:31 -0600

    added a new file and branch

---
+++ src/GoodbyeWorld.java
@@ -0,0 +1,13 @@
+
+public class GoodbyeWorld {
+
+	public GoodbyeWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Goodbye cruel world");
+
+	}
+
+}
